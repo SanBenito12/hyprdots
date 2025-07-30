73ce88b05c2e3ea4281a6ee888048661fea84791
SanBenito12 <sedc.castillo@gmail.com>
2025-07-30 23:19:48 +0300

    Theme improvements, wiremix and clear config
    * Audio mixer replaced with wiremix.
    * Window themes adapted to theme system.
    * Terminal adapted to theme system.
    * New WIP performance theme (Simple) added.
    * Scripts folder cleared.
    * Hyprland config cleared and separeted to multiple files.

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ yay -Syu --noconfirm --needed >/dev/null 2>&1 || error "System update failed"
 PACKAGES=(
   breeze cliphist git nwg-look qt6ct fish power-profiles-daemon fastfetch ttf-jetbrains-mono-nerd ttf-jetbrains-mono
   ttf-fira-code otf-fira-code-symbol hyprland yazi micro rofi-wayland hyprlock hyprpolkitagent unzip hyprsunset rofimoji
-  hyprpaper wlogout foot papirus-icon-theme base-devel waybar swaync mpv hyprpicker eww pamixer hypridle udiskie
+  hyprpaper wlogout foot papirus-icon-theme base-devel waybar swaync mpv hyprpicker eww pamixer hypridle udiskie wiremix
   network-manager-applet pamixer brightnessctl swww bibata-cursor-theme catppuccin-gtk-theme-mocha ttf-material-design-iconic-font
 )
 
