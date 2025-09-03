06cee32428bfab939422d3855a238832469a0bc8
SanBenito12 <sedc.castillo@gmail.com>
2025-09-03 12:05:47 +0300

    Add gruvbox theme

---
+++ config/themes/Gruvbox/theme.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -r ~/.config/hypr/styles/waybar/default.css ~/.config/waybar/style.css
+cp -r ~/.config/hypr/styles/waybar/defaultConfig ~/.config/waybar/config
+
+bash ~/.config/scripts/wallpaper -s ~/.config/hypr/wallpapers/Gruvbox.png
