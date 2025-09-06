7570fc02215f27c36d70128b85c74568646dfafc
SanBenito12 <sedc.castillo@gmail.com>
2025-09-07 00:49:51 +0300

    MPD adjustments and fix for gruvbox wiremix theme

--- config/themes/YellowDark/theme.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp -r ~/.config/hypr/styles/waybar/default.css ~/.config/waybar/style.css
-cp -r ~/.config/hypr/styles/waybar/defaultConfig ~/.config/waybar/config
-
-bash ~/.config/scripts/wallpaper -s ~/.config/hypr/wallpapers/Mountains.jpg
